679d0d5975d0e84b64b54928631360fbdbe34b9d
mahadihasan2 <mahadihasan125643@gmail.com>
2022-05-13 21:31:46 +0600

    show the mongodb data to client side

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from './components/Home/Home';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       {/* <h1>This is the React Router App</h1> */}
 
       <Routes>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Home = () => {
+    const [users, setUsers] = useState([])
+    useEffect(() => {
+        fetch('http://localhost:5000/user')
+            .then(res => res.json())
+            .then(data => setUsers(data))
+    }, [])
     return (
         <div>
-            <h2>This is Home</h2>
+            <h2>This Avalliable data length is: {users.length}</h2>
+            <ul>
+                {
+                    users.map(user => <li
+                        key={user._id}>Name:{user.name},
+                        E-mail:{user.email}
+                        <button>X</button>
+                    </li>)
+                }
+            </ul>
         </div>
     );
 };
