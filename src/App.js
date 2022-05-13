ff64cd21e8a158b07269cc13a79ac993e11bc175
mahadihasan2 <mahadihasan125643@gmail.com>
2022-05-13 22:46:12 +0600

    updating data to server side

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import AdUser from './components/AdUser/AdUser';
 import Home from './components/Home/Home';
+import UpdateUser from './components/UpdateUser/UpdateUser';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/user/add' element={<AdUser></AdUser>}></Route>
+        <Route path='/update/:id' element={<UpdateUser></UpdateUser>}></Route>
       </Routes>
     </div>
   );
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     const [users, setUsers] = useState([])
@@ -7,6 +8,29 @@ const Home = () => {
             .then(res => res.json())
             .then(data => setUsers(data))
     }, [])
+
+    const handleUserData = id => {
+        const proceed = window.confirm('are you sure you want to delete')
+        if (proceed) {
+            console.log('deleting user with id', id)
+            const url = `http://localhost:5000/user/${id}`
+            fetch(url, {
+                method: 'DELETE',
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data)
+                    if (data.deletedCount > 0) {
+                        const remaning = users.filter(user => user._id !== id)
+                        setUsers(remaning)
+
+                    }
+                })
+
+        }
+
+
+    }
     return (
         <div>
             <h2>This Avalliable data length is: {users.length}</h2>
@@ -15,7 +39,8 @@ const Home = () => {
                     users.map(user => <li
                         key={user._id}>Name:{user.name},
                         E-mail:{user.email}
-                        <button>X</button>
+                        <Link to={`/update/${user._id}`}><button>Update</button></Link>
+                        <button onClick={() => handleUserData(user._id)}>X</button>
                     </li>)
                 }
             </ul>
